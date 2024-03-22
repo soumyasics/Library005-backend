11d347afab1dfd701fe8780a3a393b9e07c6c406
Mojithmj <mojithmj@gmail.com>
2024-03-22 14:47:21 +0530

    message

--- Route/route.js
+++ Route/route.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const route = express.Router()
 const staffController = require('../Controller/StaffControl')
+const student=require('../Controller/Studentcontroller');
 
 route.post('/addstaff',staffController.AddStaff)
 route.post('/find',staffController.viewOne)
+
+
+route.post('/Sregister',student.StudentRegister);
+route.post('/Slogin',student.Studentlogin);
+route.get('/all',student.allview);
+route.post('/delone/:id',student.deletedata);
 
 module.exports=route
